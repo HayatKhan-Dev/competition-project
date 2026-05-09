75a984083ae387c567763502b665365f68e83474
Hayat Khan <hayatkhan808780a@gmail.com>
2026-05-09 02:39:58 -0700

    Created Hero and Header

---
+++ script.js
@@ -0,0 +1,21 @@
+const themeToggle = document.getElementById("themeToggle");
+
+// Load saved theme
+window.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "light") {
+    document.body.classList.add("light");
+  }
+});
+
+// Toggle theme
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+
+  if (document.body.classList.contains("light")) {
+    localStorage.setItem("theme", "light");
+  } else {
+    localStorage.setItem("theme", "dark");
+  }
+});
