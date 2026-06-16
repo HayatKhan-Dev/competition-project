813762d3dca2730e566dfa482f1808b62ee283c3
Hayat Khan <hayatkhan808780a@gmail.com>
2026-06-15 21:37:37 -0700

    created a token modal

--- script.js
+++ script.js
@@ -48,37 +48,37 @@ const chatOverlay = document.querySelector(".chat-overlay");
 const chatPanel = document.querySelector(".chat-panel-div");
 const closeChatBtn = document.querySelector(".close-chat-btn");
 
-chatBtn.addEventListener('click', () => {
+chatBtn.addEventListener("click", () => {
   chatOverlay.classList.add("active");
   chatPanel.classList.add("active");
 });
 
-closeChatBtn.addEventListener('click', () => {
+closeChatBtn.addEventListener("click", () => {
   chatOverlay.classList.remove("active");
   chatPanel.classList.remove("active");
 });
 
-chatOverlay.addEventListener('click', () => {
+chatOverlay.addEventListener("click", () => {
   chatOverlay.classList.remove("active");
-  chatPanel.classList.remove('active')
+  chatPanel.classList.remove("active");
 });
 
 // Dynamic Message Creation For Chatbot And User
-const chatInput = document.getElementById('chat-input');
-const sendBtn = document.querySelector('.send-btn');
-const messageArea = document.querySelector(".message-area")
+const chatInput = document.getElementById("chat-input");
+const sendBtn = document.querySelector(".send-btn");
+const messageArea = document.querySelector(".message-area");
 
 const sendMessage = () => {
-let userChatValue = chatInput.value.trim();
+  let userChatValue = chatInput.value.trim();
   if (userChatValue === undefined || userChatValue === "") {
     userChatValue = "";
   } else {
-    let userDiv = document.createElement('div');
+    let userDiv = document.createElement("div");
     userDiv.textContent = userChatValue;
     userDiv.classList.add("user-message");
     messageArea.appendChild(userDiv);
     // Bot Typing Affect
-    let typingDiv = document.createElement('div');
+    let typingDiv = document.createElement("div");
     typingDiv.textContent = "QueueX is typing...";
     typingDiv.classList.add("bot-message");
     messageArea.appendChild(typingDiv);
@@ -89,43 +89,211 @@ let userChatValue = chatInput.value.trim();
       let botResponse = getBotResponse(userChatValue.toLowerCase());
       let botDiv = document.createElement("div");
       botDiv.textContent = botResponse;
-      botDiv.classList.add('bot-message');
+      botDiv.classList.add("bot-message");
       messageArea.appendChild(botDiv);
     }, 1000);
 
-    chatInput.value = ""
+    chatInput.value = "";
   }
-}
+};
 
-function getBotResponse (userMessage) {
-  if (userMessage.includes("hello") || userMessage.includes("hi") || userMessage.includes("hey")) {
-    return "Hello, Welcome To The QueueX, How Can I Help You Today!"
+function getBotResponse(userMessage) {
+  if (
+    userMessage.includes("hello") ||
+    userMessage.includes("hi") ||
+    userMessage.includes("hey")
+  ) {
+    return "Hello, Welcome To The QueueX, How Can I Help You Today!";
   } else if (userMessage.includes("you")) {
-    return "I'm doing great. Thanks for asking, How you doing!!"
+    return "I'm doing great. Thanks for asking, How you doing!!";
   } else if (userMessage.includes("great") || userMessage.includes("fine")) {
-    return "Good to hear that, What can i assist you with!"
+    return "Good to hear that, What can i assist you with!";
   } else if (userMessage.includes("token")) {
-    return "You can get the token by clicking on the get token button in homepage"
+    return "You can get the token by clicking on the get token button in homepage";
   } else if (userMessage.includes("queue")) {
-    return "You can join the queue directly from the dashboard"
+    return "You can join the queue directly from the dashboard";
   } else if (userMessage.includes("track")) {
-    return "You can track your queue position in real time using QueueX."
+    return "You can track your queue position in real time using QueueX.";
   } else if (userMessage.includes("contact", "support", "help")) {
-    return "Our support team is available to assist you with any issues."
+    return "Our support team is available to assist you with any issues.";
   } else {
-    return "I'm sorry, I didn't understand that question. Try asking about tokens, queues, or tracking."
+    return "I'm sorry, I didn't understand that question. Try asking about tokens, queues, or tracking.";
   }
 }
 
-sendBtn.addEventListener('click', sendMessage);
-chatInput.addEventListener('keydown', (e) => {
+sendBtn.addEventListener("click", sendMessage);
+chatInput.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     sendMessage();
   }
 });
 
 // Token Modal
+const serviceDropDown = document.getElementById("service");
+const userName = document.getElementById("name-input");
+const userPhone = document.getElementById("phone");
+const generateButton = document.querySelector(".generate-btn");
+const tokenResult = document.querySelector(".token-result");
 
+generateButton.addEventListener("click", () => {
+  serviceDropDown.classList.remove("error");
+  userName.classList.remove("error");
+  userPhone.classList.remove("error");
+
+  let serviceVal = serviceDropDown.value.trim();
+  let name = userName.value.trim();
+  let phoneNo = userPhone.value.trim();
+
+  if (serviceVal === "Default") return serviceDropDown.classList.add("error");
+  if (name === "") return userName.classList.add("error");
+  if (phoneNo === "") return userPhone.classList.add("error");
+
+  let tokenPrefix;
+  let waitTime;
+  if (serviceVal === "Bank") {
+    tokenPrefix = "B";
+    waitTime = "10 mins";
+  } else if (serviceVal === "Government Office") {
+    tokenPrefix = "G";
+    waitTime = "31 mins";
+  } else if (serviceVal === "Hospital") {
+    tokenPrefix = "H";
+    waitTime = "20 mins";
+  } else if (serviceVal === "Customer Support") {
+    tokenPrefix = "S";
+    waitTime = "10 mins";
+  }
+
+  let randomNum = Math.floor(Math.random() * 999 + 1);
+  let formattedNum = String(randomNum).padStart(3, "0");
+  let finalToken = tokenPrefix + "-" + formattedNum;
+
+  tokenResult.innerHTML = `<div class="result-header">
+        <i class="fa-regular fa-circle-check"></i>
+        <h3>Token Generated Successfully</h3>
+      </div>
+      <div class="result-info">
+        <div class="first-div">
+          <span>Service:</span>
+          <h4>${serviceVal}</h4>
+        </div>
+        <div>
+          <span>Name:</span>
+          <h4>${name}</h4>
+        </div>
+        <div>
+          <span>Phone:</span>
+          <h4>${phoneNo}</h4>
+        </div>
+        <div>
+          <span>Token Number:</span>
+          <h4>${finalToken}</h4>
+        </div>
+        <div class="last-div">
+          <span>Estimated Wait Time:</span>
+          <h4>${waitTime}</h4>
+        </div>
+      </div>
+    </div>
+    <div class="result-actions">
+  <button class="back-btn">
+    Generate Another Token
+  </button>
+</div>`;
+
+const backBtn = document.querySelector(".back-btn");
+
+backBtn.addEventListener("click", () => {
+  tokenResult.classList.remove("active");
+  tokenModal.classList.add("active");
+
+  gsap.fromTo(
+    tokenModal,
+    {
+      opacity: 0,
+      scale: 0.85,
+      y: 20,
+    },
+    {
+      opacity: 1,
+      scale: 1,
+      y: 0,
+      duration: 0.4,
+      ease: "power3.out",
+    }
+  );
+});
+
+  tokenModal.classList.remove("active");
+  tokenResult.classList.add("active");
+  gsap.fromTo(
+    tokenResult,
+    {
+      opacity: 0,
+      scale: 0.8,
+      y: 30,
+    },
+    {
+      opacity: 1,
+      scale: 1,
+      y: 0,
+      duration: 0.5,
+      ease: "back.out(1.7)",
+    },
+  );
+  serviceDropDown.value = "";
+  userName.value = "";
+  userPhone.value = "";
+});
+
+// Token Modal Opening/Closing code...
+const tokenOverlay = document.querySelector(".token-overlay");
+const tokenModal = document.querySelector(".token-modal");
+const closeTokenBtn = document.querySelector(".close-token-btn");
+const getTokenBtn = document.querySelector("#get-token");
+const joinQueueBtn = document.querySelector(".join-btn");
+
+function openModal() {
+  tokenResult.classList.remove("active");
+  tokenOverlay.classList.add("active");
+  tokenModal.classList.add("active");
+
+  gsap.fromTo(
+    tokenModal,
+    {
+      opacity: 0,
+      scale: 0.85,
+      y: 20,
+    },
+    {
+      opacity: 1,
+      scale: 1,
+      y: 0,
+      duration: 0.4,
+      ease: "power3.out",
+    },
+  );
+}
+
+function closeModal() {
+  gsap.to(tokenModal, {
+    opacity: 0,
+    scale: 0.85,
+    y: 20,
+    duration: 0.3,
+    ease: "power3.in",
+    onComplete: () => {
+      tokenOverlay.classList.remove("active");
+      tokenModal.classList.remove("active");
+      tokenResult.classList.remove("active");
+    },
+  });
+}
+
+getTokenBtn.addEventListener("click", openModal);
+joinQueueBtn.addEventListener("click", openModal);
+closeTokenBtn.addEventListener("click", closeModal);
+tokenOverlay.addEventListener("click", closeModal);
 
 // GSAP Animations
 document.addEventListener("DOMContentLoaded", () => {
@@ -180,12 +348,16 @@ document.addEventListener("DOMContentLoaded", () => {
       { duration: 1, y: 50, opacity: 0, ease: "power2.out" },
       "-=0.5",
     );
-    tl.from(".live-queue", {
-      duration: 0.5,
-      y: 20,
-      opacity: 0,
-      ease: "power2.out",
-    },"-=0.5");
+    tl.from(
+      ".live-queue",
+      {
+        duration: 0.5,
+        y: 20,
+        opacity: 0,
+        ease: "power2.out",
+      },
+      "-=0.5",
+    );
     tl.from(
       ".queue-row",
       { duration: 1, x: 50, opacity: 0, ease: "power2.out", stagger: 0.2 },
@@ -215,8 +387,8 @@ document.addEventListener("DOMContentLoaded", () => {
         containerAnimation: horizontalScroll,
         start: "left 60%",
         toggleActions: "play none none reset",
-      }
-    })
+      },
+    });
 
     aboutTl.from(".about-heading", {
       y: 50,
@@ -240,7 +412,7 @@ document.addEventListener("DOMContentLoaded", () => {
       y: 30,
       opacity: 0,
       duration: 0.5,
-    })
+    });
 
     aboutTl.from(".point-icon", {
       x: 30,
@@ -248,11 +420,15 @@ document.addEventListener("DOMContentLoaded", () => {
       duration: 0.5,
     });
 
-    aboutTl.from(".point-text", {
-      x: -60,
-      opacity: 0,
-      duration: 0.5,
-    }, "-=0.5");
+    aboutTl.from(
+      ".point-text",
+      {
+        x: -60,
+        opacity: 0,
+        duration: 0.5,
+      },
+      "-=0.5",
+    );
 
     aboutTl.from(".about-right", {
       x: 100,
@@ -264,7 +440,7 @@ document.addEventListener("DOMContentLoaded", () => {
       y: 30,
       opacity: 0,
       duration: 0.5,
-    })
+    });
   }
 
   // other animations after scrll trigger
@@ -283,7 +459,7 @@ document.addEventListener("DOMContentLoaded", () => {
       trigger: ".contact-section",
       start: "top 60%",
       toggleActions: "play none none reset",
-    }
+    },
   });
 
   contactTl.from(".contact-title", {
@@ -292,28 +468,40 @@ document.addEventListener("DOMContentLoaded", () => {
     duration: 0.8,
   });
 
-  contactTl.from(".contact-desc",{
-    y: 30,
-    opacity: 0,
-    duration: 0.5,
-  },"-=0.5");
+  contactTl.from(
+    ".contact-desc",
+    {
+      y: 30,
+      opacity: 0,
+      duration: 0.5,
+    },
+    "-=0.5",
+  );
 
-  contactTl.from(".info-item", {
-    x: 30,
-    opacity: 0,
-    duration: 0.8,
-    stagger: 0.2,
-  }, "-=0.5");
+  contactTl.from(
+    ".info-item",
+    {
+      x: 30,
+      opacity: 0,
+      duration: 0.8,
+      stagger: 0.2,
+    },
+    "-=0.5",
+  );
 
-  contactTl.from(".contact-dashboard", {
-    x: 100,
-    opacity: 0,
-    duration: 0.7,
-  }, "-=0.5");
-  
+  contactTl.from(
+    ".contact-dashboard",
+    {
+      x: 100,
+      opacity: 0,
+      duration: 0.7,
+    },
+    "-=0.5",
+  );
+
   contactTl.from(".contact-header", {
     y: 30,
     opacity: 0,
     duration: 0.5,
-  })
+  });
 });
